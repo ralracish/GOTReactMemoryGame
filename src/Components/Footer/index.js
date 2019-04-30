43fa6b2487421b44398c838e7f0a5b1cb2e78ec0
ralracish <carlarblackman@gmail.com>
2019-04-29 22:22:31 -0400

    files

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div>
+                <h5>Clicky Game using React</h5>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Components/Thrones/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Thrones(props) {
+    return (
+        <div class="jumbotron">
+            <div className="img-container col-md-3" onClick={() => props.z(props.id)}>
+                <img alt={props.x} src={props.y} height="150px" width="150px"/>
+            </div>
+        </div>
+    );
+}
+
+export default Thrones;
